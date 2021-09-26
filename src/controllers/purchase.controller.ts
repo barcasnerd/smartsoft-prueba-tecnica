558aa17ddda412c94e4c98803784535551306b46
= <=>
2021-09-26 15:11:52 +0000

    ci: finalize purchase controller configs

--- src/controllers/purchase.controller.ts
+++ src/controllers/purchase.controller.ts
@@ -67,3 +67,68 @@ export const createPurchase = async (req: Request, res: Response): Promise<Respo
     }
     return res.status(400).json({ msg: "Failed to save purchase" });
 };
+
+/**
+ * Gets a purchase based on the request id
+ * @param req 
+ * @param res 
+ * @returns a purchase
+ */
+export const getPurchase = async (req: Request, res: Response): Promise<Response> => {
+    const result = await getRepository(ProductPurchase).findOne(req.params.id);
+    if (result) {
+        return res.status(302).json(result);
+    }
+    return res.status(404).json({ msg: "Purchase id not found" });
+};
+
+
+/**
+ * delete a purchase based on its id
+ */
+export const deletePurchase = async (req: Request, res: Response): Promise<Response> => {
+    const result = await getRepository(ProductPurchase).delete(req.params.id);
+    if (result) {
+        return res.status(200).json(result);
+    }
+    return res.status(404).json({ msg: "Purchase id not found" });
+};
+
+
+/**
+ * Update an purchase based on request id and a product list
+ * @param req 
+ * @param res 
+ * @returns updated purchase
+ */
+export const updatePurchase = async (req: Request, res: Response): Promise<Response> => {
+    // manage typeorm queries
+    const manager = getManager()
+
+    const purchase = await getRepository(ProductPurchase).findOne(req.params.id); // purchase requested
+
+    if (purchase) { // purchase existance verifying
+        const { products } = req.body; // the purchase request recieves a list of product ids only
+        const productList = await manager.createQueryBuilder(Product, "product") // then get all products with those ids
+            .where("product.id IN (:...products)", { products: products })
+            .getMany();
+
+        let total = 0; // define the total ammount of the purchase
+        productList.forEach((product) => { // to iterate all elements of the purchase
+            total += product.price * product.quantity; // get the price by each product based on its price and quantity
+        });
+
+        // update the purchase
+        await getRepository(ProductPurchase).merge(purchase, {
+            products: productList,
+            total: total
+        });
+
+        // then save the purchase into database
+        const result = getRepository(ProductPurchase).save(purchase);
+        return res.status(200).json(result);
+    }
+
+    return res.status(404).json({ msg: "Purchase id not found" });
+
+};
--- src/routes/purchase.routes.ts
+++ src/routes/purchase.routes.ts
@@ -1,13 +1,12 @@
 import { Router } from 'express';
-import { getPurchases, createPurchase } from "../controllers/purchase.controller";
-//import { getPurchases, createPurchase, getPurchase, updatePurchase, deletePurchase } from "../controllers/purchase.controller";
+import { getPurchases, createPurchase, getPurchase, updatePurchase, deletePurchase } from "../controllers/purchase.controller";
 
 const router = Router();
 
 router.get('/', getPurchases);
 router.post('/', createPurchase);
-// router.get('/:id', getPurchase);
-// router.put('/:id', updatePurchase);
-// router.delete('/:id', deletePurchase);
+router.get('/:id', getPurchase);
+router.put('/:id', updatePurchase);
+router.delete('/:id', deletePurchase);
 
 export default router;
