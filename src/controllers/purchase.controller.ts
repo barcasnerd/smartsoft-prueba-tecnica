bcddc7666f2a87c9bc3ebdb7f4840d490c3167e3
= <=>
2021-09-26 14:41:07 +0000

    ci: add explanation comments rto the code

--- src/controllers/purchase.controller.ts
+++ src/controllers/purchase.controller.ts
@@ -19,7 +19,6 @@ export const getPurchases = async (req: Request, res: Response): Promise<Respons
     return res.status(200).json(purchases);
 };
 
-
 /**
  * Create a purchase, then save it into the product database table
  * @param req 
@@ -28,27 +27,34 @@ export const getPurchases = async (req: Request, res: Response): Promise<Respons
  * @returns the saved purchase
  */
 export const createPurchase = async (req: Request, res: Response): Promise<Response> => {
+    // manage typeorm queries
     const manager = getManager()
-    const currentUser = await getRepository(User).findOne(req.user);
 
-    const { products } = req.body;
-    const productList = await manager.createQueryBuilder(Product, "product")
+    const currentUser = await getRepository(User).findOne(req.user); // get the current user
+
+    const { products } = req.body; // the purchase request recieves a list of product ids only
+    const productList = await manager.createQueryBuilder(Product, "product") // then get all products with those ids
         .where("product.id IN (:...products)", { products: products })
         .getMany();
 
-    let total = 0;
-    productList.forEach((product) => {
-        total += product.price * product.quantity;
+    let total = 0; // define the total ammount of the purchase
+    productList.forEach((product) => { // to iterate all elements of the purchase
+        total += product.price * product.quantity; // get the price by each product based on its price and quantity
     });
 
+    // reduce the current money of the logged user
     if (currentUser && currentUser.money - total > 0) {
+        // edit the user ammount badge
         await getRepository(User).merge(currentUser, {
             money: currentUser.money - total
-        })
+        });
+        // save the updated user
+        await getRepository(User).save(currentUser);
     } else {
         return res.status(400).json({ msg: "Insufficient money" });
     }
 
+    // then create the actual purchase to save it into database
     const newPurchase = await getRepository(ProductPurchase).create({
         user: currentUser,
         products: productList,
